cf0b69dbe56310a4858637958113ff8b686d9b03
iza <59828082+izadoesdev@users.noreply.github.com>
2025-04-23 17:02:27 +0300

    fix: drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,5 +4,8 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/db/schema.ts",
   out: "./drizzle",
-  dialect: "postgresql"
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL || "",
+  },
 } satisfies Config;
